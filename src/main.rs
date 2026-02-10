ec6b9007cf6ace6a60d64113da16531b9167881c
Nicolas Garric <nicolasdfgarric@gmail.com>
2026-02-10 10:59:48 +0100

    First part of the game done && explanations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,55 @@
-use std::io;
+use std::io; // bring the io input/output library into scope.
+// The io library comes from the standard library, known as std
+// --> ability to accept user input
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!"); //  macro that prints a string to the screen
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
+    // let --> create a variable
+    //  + mut mutable variable
+    // guess --> function name
+
+    // = is used to bind the variable guess to the instance of a String
+
+    // String type
+    //  :: --> associated fonction
+    //  new associated function of the String type
+
+
+    // ------
+    // let mut guess = String::new();
+    // has created a mutable variable
+    // that is currently bound to a new, empty instance of a String
+    // ------
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // call the stdin function from the io module
+
+    // .read_line() --> take user input
+    // .read_line(&mut guess) --> take user input and bind it
+        // to mut var guess --> String type
+
+    // .expect linked to Result Enum element to handle error
+    // Result’s variants are Ok and Err
+        // If this instance of Result is an Err value,
+        // expect will cause the program to crash
+        // and display the message that you passed as an argument to expect
+
+
+
+    // io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // --> refacto
+
     println!("You guessed: {guess}");
+
+    // {} --> placeholder for the variable
+
+    // println!("You guessed: {}", guess); --> also work
+
 }
